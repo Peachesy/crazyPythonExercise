a1023bb283252c9f62f01943b0b519c153065beb
Peachesy <2360950972@qq.com>
2020-08-05 10:44:44 +0800

    Finish guessnumV1.0

--- Applications/GuessNumV1.0.py
+++ Applications/GuessNumV1.0.py
@@ -3,6 +3,8 @@
 # 比goal大的话提示用户：猜大了，目标范围在（a, guess）
 # 比goal小的话提示用户：猜小了，目标范围在（guess, b)
 # 与goal相等的话提示用户：猜数成功！
+# 每次猜完之后更新范围a, b
+
 import random
 
 print("请输入一个范围，从a到b")
@@ -14,19 +16,26 @@ print(goal)
 
 print("猜数开始")
 
+# 定义一个列表储存用户猜的数字
+guess = []
 # 用count来记录用户猜数轮数
-length  = int(b-a)
+count = 1
+length = int(b-a)
+print(range(length))
 for i in range(length):
-    guess = int(input("请猜数："))
-    if guess > goal:
-        print("猜大了，范围在：，" % a, guess)
-        count = count+1
-    if guess < goal:
-        print("猜小了，范围在：，" % guess, b)
+    guess.append(int(input("请猜数：")))
+    if guess[i] > goal:
+        b = guess[i]
+        print("猜大了，范围在：%d, %d" % (a, b))
         count = count + 1
-    if guess == goal:
+    if guess[i] < goal:
+        a = guess[i]
+        print("猜小了，范围在：%d, %d" % (a, b))
+        count = count + 1
+    if guess[i] == goal:
         print("恭喜猜数成功！")
         break
-
+print(guess)
+print(count)
 
 
